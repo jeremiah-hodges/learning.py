7f041b339972779635ad6c7a543b63ebb3aebe72
Jeremiah <jeremiah.hodges@duke.edu>
2021-06-12 19:24:56 -0500

    Initial commit

---
+++ nums.py
@@ -0,0 +1,28 @@
+# importing libraries
+from math import *
+
+
+
+print( 3 +4)
+x = 9-1
+
+# str(num) wraps int as a string required when printing both numbers and strings
+print("i like the number" + str(x))
+
+# absolute value
+print(abs(-90))
+
+# exponents
+print(pow(2, 2))
+
+# max/min
+print(min(10, 3))
+print(max(2, 20))
+
+# rounding
+print(round(5.2932323))
+
+# floor (round down)
+print(floor(5.992342340))
+
+# ceil (round up)
---
+++ text.py
@@ -0,0 +1,26 @@
+
+name = "Alex"
+age = "24"
+print("Hello " + name + " are you " + age + "?")
+
+print("Monkey \nAcademy")
+
+print("\" hello.... \" the girl said")
+
+# change string to all upper or lower (string.upper())
+s = "HELLO HOW ARE YOU DOING TODAY";
+print(s.lower())
+
+
+# find length of string
+x = len(s)
+print(x)
+
+# Select specified index
+print(s[2])
+
+# Find first index of specified character
+print(s.index("H"))
+
+# Replace characters in a string
+print(s.replace("H", "ZSDF"))
